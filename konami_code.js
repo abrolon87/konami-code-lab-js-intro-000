6118540709915653fbe0310ddf785efd3e06d714
Amanda R <abrolon87@gmail.com>
2019-08-24 18:35:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,31 +12,6 @@ const codes = [
 ];
 
 
-/*
-function init() {
-  let index = 0;
-  function onKeyDownHandler(e) {
-    const key = e.key;
-    if (codes[index] === key) {
-     for (let i = 0, l = codes.length; i < l; i++) {
-            onKeyDownHandler(codes[i]);
-          }
-
-      if (index === codes.length) {
-        for (let i = 0, l = codes.length; i < l; i++) {
-                onKeyDownHandler(codes[i]);
-              }
-
-        alert("Victory!");
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  }
-}
-*/
 function init() {
   var index = 0;
   document.body.addEventListener('keydown', onKeyDownHandler);
