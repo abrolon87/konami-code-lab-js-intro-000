402865802422748e8d476507ad3438dd6978c237
Amanda R <abrolon87@gmail.com>
2019-08-24 18:06:50 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+  let index = 0;
+  function onKeyDownHandler(e) {
+    const key = e.key;
+
+    if (codes[index] === key) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Victory!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
